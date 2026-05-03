e4256b9aa2c25f7500cef1e5d5b2edd157ce6b46
M-Bilal-15302 <bilalkhattak0318153@gmail.com>
2026-05-04 00:11:39 +0500

    add try & catch with example

--- this_keyword.js
+++ this_keyword.js
@@ -17,6 +17,33 @@ const student = {
     console.log(avg);
   },
 };
-console.log(student.stdName);
-console.log(student.age);
-student.getAvg();
+// console.log(student.stdName);
+// console.log(student.age);
+// student.getAvg();
+
+//  --------------------Try & Catch -----------
+// Try statement allows you to define a block of code to be tested for errors while it is being executed.
+
+// try{
+//    here will be a block of code where you think an error could occurs.
+// }
+
+// -----------------------catch
+// the catch statement allows you to define a block of code to be exwcuted, if an error occure in try block.
+
+// --------------- example 1
+// console.log("hello before error");
+// console.log(a);
+// console.log("hello after error");
+
+//  ---------------Example 2
+console.log("hello before try");
+try {
+  console.log(a);
+} catch (e) {
+  // e is like an perameter to catch that store the error.
+  console.log("a is not defined");
+  console.log(e);
+}
+console.log("hello after catch");
+// in example 2 the code after error in try block will be executed normally.
