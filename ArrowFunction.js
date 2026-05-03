f6c6d7b8df60083651cd0ff3c1af7cce6b3d78b9
M-Bilal-15302 <bilalkhattak0318153@gmail.com>
2026-05-04 00:32:01 +0500

    Arrow function with definition and example

---
+++ ArrowFunction.js
@@ -0,0 +1,10 @@
+//  ----------------- Arrow function-----
+//  arrow fucntion is a function without function keyword and it is stored in a variable.
+//  usually arrow function is used for small function definition.
+//  it is used for callback function(those function which passed as argument to another function).
+//  since we dont want to change the value of arrow function variable so it is best to declare it with const.
+
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(5, 6));
