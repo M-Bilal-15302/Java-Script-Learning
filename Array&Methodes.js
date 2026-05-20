a0e2eaa0b5339828d9259ba224575354eabc2e0f
M-Bilal-15302 <bilalkhattak0318153@gmail.com>
2026-05-20 21:02:11 +0500

    forEach of array

---
+++ Array&Methodes.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5];
+
+// forEach is highorder function that take callback function and you can perform a specific
+// task for each of array element.
+//  we can use as loop that run for each element of array
+arr.forEach((i) => {
+  console.log(`do something for element ${i}`);
+});
