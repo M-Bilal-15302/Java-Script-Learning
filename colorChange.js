85dd82d2a77b6b61e7454ed5393592ea24bd8025
M-Bilal-15302git <bilalkhattak0318153@gmail.com>
2026-06-15 14:12:01 +0500

    small change color activity using dom eventListner

---
+++ colorChange.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+let heading = document.querySelector("h1");
+let div = document.querySelector("div");
+
+btn.addEventListener("click", () => {
+  const color = getColor();
+  heading.innerText = getColor();
+  div.style.backgroundColor = color;
+  //   document.body.style.backgroundColor = color;
+  //   if (heading) heading.textContent = color;
+});
+
+function getColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  //   console.log(`${red}, ${green}, ${blue}`);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
